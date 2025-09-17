189cb3a3c1b1a189df68f65fabfb5aa632fc4635
user <user@DESKTOP-0FTCJBS>
2025-09-17 21:28:38 +0300

    Actions Return Types

--- MVCSession2G01/Controllers/MovieController.cs
+++ MVCSession2G01/Controllers/MovieController.cs
@@ -2,18 +2,94 @@
 
 namespace MVCSession2G01.Controllers
 {
-    // MVC Controller
-    public class MovieController :Controller
+    public class MovieController : Controller
     {
-        //Actions : Public non-static method
-        public string GetMovie(int id)
+        // Actions : public non-static method  
+        // Actions : Have Special Return Types
+        // ActionResult 
+
+
+        //public string GetMovie(int id)
+        //{
+        //    return $"Get Movie With Id: {id} ";
+        //}
+
+        //public string AddMovie(int id)
+        //{
+        //    return $"Add Movie With Id: {id} ";
+        //}
+
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var contentResult = new ContentResult()
+        //    { 
+        //        Content = $"Add Movie With Id: {id}",
+        //        ContentType = "text/html",
+        //        StatusCode = 200
+        //    };
+
+        //    return contentResult;
+
+        //}
+
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var contentResult = new ContentResult()
+        //    {
+        //        Content = $"Add Movie With Id: {id}",
+        //        ContentType = "object/pdf",
+        //        StatusCode = 200
+        //    };
+
+        //    return contentResult;
+
+        //}
+
+        //public RedirectResult GetMovie(int id)
+        //{
+        //    var redircetResult = new RedirectResult("https://google.com/");
+
+        //    return redircetResult;
+
+        //}
+
+        //public RedirectToActionResult GetMovie(int id)
+        //{
+        //    var redirectToActionResult = new RedirectToActionResult("AddMovie", "Movie", new { id = 133 });
+
+        //    return redirectToActionResult;
+
+        //}
+
+
+
+        // Any Action I Will Create in Mvc Controller html El Returnt Type IActionResult
+        [ActionName("GetMovie")]
+        public IActionResult GetMovieByIdWithCategoryDramaAndPoster(int id)
         {
-            return $"Get Movie With Id: {id}";
-        }
-        public string AddMovie(int id)
-        {
-            return $"Add Movie With Id: {id}";
+            if (id == 1)
+            {
+                //return new ContentResult() { Content = $"Add Movie With Id: {id}", ContentType = "text/html" };
+                return Content($"Add Movie With Id: {id}", "text/html");
+            } 
+            else if (id == 2)
+            {
+                //return new ContentResult(){ Content = $"Add Movie With Id: {id}", ContentType = "object/pdf" };
+                return Content($"Add Movie With Id: {id}", "object/pdf");
+            } 
+            else if (id == 3)
+            {
+                // return new RedirectResult("https://google.com/");
+                return Redirect("https://google.com/");
+            }
+            
+            else
+            {
+               // return new RedirectToActionResult("AddMovie", "Movie", new { id = 133 });
+                return RedirectToAction("AddMovie", "Movie", new { id = 133 });
+            }
         }
 
+        
     }
 }
--- MVCSession2G01/Program.cs
+++ MVCSession2G01/Program.cs
@@ -19,7 +19,7 @@ namespace MVCSession2G01
             // MVC
             app.MapControllerRoute(
                 name:"default",
-                pattern:"{controller=Movie}/{action=GetMovie}/{id:alpha:length(5)=12}"
+                pattern:"{controller=Movie}/{action=GetMovie}/{id}"
                 );
 
 
