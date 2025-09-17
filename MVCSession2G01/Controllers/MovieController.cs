d081fb9e1adcadf0e35971234703e89b2e6aac1a
user <user@DESKTOP-0FTCJBS>
2025-09-17 19:33:35 +0300

    Add project files.

---
+++ MVCSession2G01/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSession2G01.Controllers
+{
+    // MVC Controller
+    public class MovieController :Controller
+    {
+        //Actions : Public non-static method
+        public string GetMovie(int id)
+        {
+            return $"Get Movie With Id: {id}";
+        }
+        public string AddMovie(int id)
+        {
+            return $"Add Movie With Id: {id}";
+        }
+
+    }
+}
---
+++ MVCSession2G01/Program.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace MVCSession2G01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();//Register Built-in MVC Services
+            
+            var app = builder.Build();
+
+            // app.MapGet("/", () => "Hello World!");
+            //// app.MapGet("/login", () => "You Are Sign In!");
+            // app.MapGet("/login", () => Signin);
+
+            // MVC
+            app.MapControllerRoute(
+                name:"default",
+                pattern:"{controller=Movie}/{action=GetMovie}/{id:alpha:length(5)=12}"
+                );
+
+
+            app.Run();
+        }
+
+        public static string Signin()
+        {
+            return $"You Are Sign In!";
+        }
+
+        
+    }
+}
